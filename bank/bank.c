f8545a16890f3ba22ba781c1a035825a82ea6175
Chaggar25 <rajvirchaggar@hotmail.com>
2026-04-05 16:31:24 -0700

    added and verified the loop statement

--- bank/bank.c
+++ bank/bank.c
@@ -4,19 +4,45 @@
 int main() {
     int balance = 1000;
     int choice;
+    int depositAmt;
+    int i;
 
-    printf("**** Welcome to the D Bank ****\n");
-    printf("1. Check Balance\n");
-    printf("2. Deposit\n");
-    printf("3. Exit\n");
+    // printf("**** Welcome to the D Bank ****\n");
+    // printf("1. Check Balance\n");
+    // printf("2. Deposit\n");
+    // printf("3. Create new Account\n");
+    // printf("4. Exit\n");
 
-    printf("Enter your choice: ");
-    scanf("%d", &choice);
+    // printf("Enter your choice: ");
+    // scanf("%d", &choice);
 
-    if (choice == 1){
-        printf("Your balance is: %d\n", balance);
+    while(choice != 4){
+        printf("**** Welcome to the D Bank ****\n");
+        printf("1. Check Balance\n");
+        printf("2. Deposit\n");
+        printf("3. Create new Account\n");
+        printf("4. Exit\n");
+
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+
+        if (choice == 1){
+            printf("Your balance is: %d\n", balance);
+        } else if (choice == 2){
+            printf("Enter amount to deposit\n: ");
+            scanf("%d", &depositAmt);
+            if(depositAmt > 0){
+                depositAmt += balance;
+                printf("amount update: %d\n", depositAmt);
+                balance = depositAmt;
+            }
+        }
     }
 
     return 0;
+
+
+
     
 }
